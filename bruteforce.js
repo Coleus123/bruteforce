28116bbd0d5084f43448f37443c1668bf31eb711
Coleus123 <146657138+Coleus123@users.noreply.github.com>
2023-12-17 22:00:50 +0500

    Update bruteforce.js

--- bruteforce.js
+++ bruteforce.js
@@ -4,29 +4,39 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
-
 rl.question('Введите первую строку: ', (str1) => {
   rl.question('Введите вторую строку: ', (str2) => {
-    const count = bruteForceSearch(str1, str2);
-    console.log(`Количество вхождений: ${count}`);
+    const results = bruteForceSearch(str1, str2);
+    console.log(`Количество вхождений: ${results}`);
     rl.close();
   });
 });
 
 function bruteForceSearch(str1, str2) {
-  let count = 0;
+  let results = new Array();
   const hash1 = hash(str1);
 
   for (let i = 0; i <= str2.length - str1.length; i++) {
     const substring = str2.substring(i, i + str1.length);
     const subhash = hash(substring);
-
-    if (subhash === hash1 && substring === str1) {
-      count++;
+    if (subhash == hash1){
+		for(let j = 0; j < str1.length; j++){
+			if (substring[j] === str1[j]){
+				if (j == str1.length - 1){
+					results.push(i);
+				}
+				else{
+				continue;
+				}
+			}
+			else {
+				break;
+			}
+			
+		}
     }
   }
-
-  return count;
+  return results;
 }
 
 function hash(str) {
